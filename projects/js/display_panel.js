34e77ad531ce84a49f915bb1cae9527321b57d43
xavier.belenus <xavier.belenus@gmail.com>
2023-10-23 10:53:47 +0200

    building side panel

---
+++ projects/js/display_panel.js
@@ -0,0 +1,6 @@
+/* CONTROL PANEL 
+  mouseEvent
+  localStorage
+  temps de visite (durée d'activité)
+  vitesse de connexion
+*/
--- projects/js/display_stack.js
+++ projects/js/display_stack.js
@@ -1,26 +1,46 @@
-/* 
-MA STACK
+// CARD HEADER |> stackPanel.querySelector(".uk-card-header")
+// CARD BODY |> stackPanel.querySelector(".uk-card-body")
 
-Vanilla/Nodejs
-Mongodb
-mysql
-Typescript
-React
-Vue
-UIkit
+/* PANEL CONTAINER */
+let flexDiv = document.createElement('div')
+flexDiv.style.display = "flex"
+flexDiv.style.flexDirection = "row"
+flexDiv.style.flexWrap = "wrap"
 
-ajouter des animation avec delay 700ms
-*/
-
+let stackFront = ["Vanilla / Nodejs","Typescript","React","Vue","UIkit"]
 let sideMenu = document.querySelector('.inside-side-menu')
 let stackPanel = createPanel()
-stackPanel.querySelector(".uk-card-header").textContent = "Stack Javascript"
-stackPanel.querySelector(".uk-card-body").textContent = `Vanilla/Nodejs
-Mongodb
-mysql
-Typescript
-React
-Vue
-UIkit`
-//ORGANISER EN SECTIONS
-sideMenu.insertBefore(stackPanel, sideMenu.querySelector('.resize'))
+stackPanel.style.marginRight = "30px"
+stackPanel.style.flex = "1"
+stackPanel.style.minWidth = "250px"
+stackPanel.style.maxWidth = "300px"
+stackPanel.querySelector(".uk-card-header").textContent = "Stack Frontend"
+
+let list = document.createElement('ul')
+list.classList.add("uk-list", "uk-list-hyphen")
+stackPanel.querySelector(".uk-card-body").appendChild(list)
+flexDiv.appendChild(stackPanel)
+sideMenu.insertBefore(flexDiv, sideMenu.querySelector('.resize'))
+stackFront.forEach((el,i) =>{
+  let li = document.createElement('li')
+  i % 2 === 0 ? li.classList.add('uk-animation-slide-right') : li.classList.add('uk-animation-slide-left')
+  li.textContent = el
+  list.appendChild(li)
+})
+
+let stackBack = ["PHP", "Symphony", "MySQL", "Mongodb", "Rust"]
+let stackPanel2 = createPanel()
+stackPanel2.style.flex = "1"
+stackPanel2.style.minWidth = "250px"
+stackPanel2.style.maxWidth = "300px"
+stackPanel2.querySelector(".uk-card-header").textContent = "Stack Backend"
+let list2 = document.createElement('ul')
+list2.classList.add("uk-list", "uk-list-hyphen")
+stackPanel2.querySelector(".uk-card-body").appendChild(list2)
+flexDiv.appendChild(stackPanel2)
+stackBack.forEach((el, i) => {
+  let li = document.createElement('li')
+  i % 2 === 0 ? li.classList.add('uk-animation-slide-right') : li.classList.add('uk-animation-slide-left')
+  li.textContent = el
+  list2.appendChild(li)
+})
